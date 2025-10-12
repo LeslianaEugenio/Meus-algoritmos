7100b647978b268e635189c7f25b98040d9d5038
Lesliana Eugénio <leslianaeugenio48@gmail.com>
2025-10-12 19:28:23 +0100

    Mais um dia de estudos

--- src/src/algoritmos/VendasSoftware.java
+++ src/src/algoritmos/VendasSoftware.java
@@ -1,7 +1,22 @@
 package algoritmos;
 
+import java.util.Scanner;
+
 public class VendasSoftware {
     static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("====Vendas de Software=====");
+        double salario = 75.000;
+        double valorDeVendas;
+        System.out.println("Digite o valor de vendas de software no mês: ");
+        valorDeVendas = input.nextDouble();
+
+        valorDeVendas = (valorDeVendas * 15)/100;
+        double salarioTotal = valorDeVendas + salario;
+
+        System.out.println("15% do seu valor de vendas: " + valorDeVendas);
+        System.out.printf(" O seu salario total com a comissão de 15%% pelo valor de vendas é: %.3f",
+                salarioTotal, "Mil kwanzas");
 
     }
 }
